a79abdc3ab6ccf31599f84e123275c58d3eeb67c
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-02-13 19:57:07 -0500

    [FIX] cors desde el frontend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -10,7 +11,21 @@ const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded())
+
+app.use(cors({
+  origin: '*',
+  methods: "GET,POST,PUT,DELETE",
+}))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', routes())
+
+app.use(express.static('uploads'));
 
 const conectarDB = async () => {
   try {
