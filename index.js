d36c8f1b00c002fe9000626daa47b6b609b6d3ce
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-02-15 14:58:18 -0500

    [ADD] files necesarios para deployar

--- index.js
+++ index.js
@@ -12,16 +12,29 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded())
 
+const whitelist = [process.env.FRONTEND_URL]
+// console.log('whitelist', whitelist)
+// (origin, callback) => {
+//   if (whitelist.includes(origin)) {
+//     callback(null, true)
+//   } else {
+//     console.log('origin:', origin, 'not allowed')
+//     callback(new Error('Not allowed by CORS'))
+//   }
+// }
 app.use(cors({
-  origin: '*',
-  methods: "GET,POST,PUT,DELETE",
+  origin: whitelist
 }))
+// Guía 2022 para habilitar cors: https://www.workfall.com/learning/blog/how-to-configure-cors-in-node-js-with-express/
+// methods: "POST,PUT,DELETE",
+// origin: ['https://www.workfall.com', 'https://www.google.com'],
+// origin: 'http://localhost:5173',
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use('/', routes())
 
@@ -36,8 +49,9 @@ const conectarDB = async () => {
   }
 }
 
-let PORT = '5000'
-app.listen(PORT, () => console.log(`servidor arrancado en puerto ${PORT}`))
+const HOST = process.env.HOST || '0.0.0.0'
+let PORT = process.env.POST || '5000'
+app.listen(PORT, HOST, () => console.log(`servidor arrancado en puerto ${PORT}`))
 
 conectarDB()
 // console.log(process.env.MONGODB_CONNECTION_STRING)
